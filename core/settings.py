854145d8bbd26ce17d6e711d8d8ace4064d6768c
L0czi <m.tesarski55@gmail.com>
2021-09-15 09:36:14 +0200

    add docker and postreSQL

--- core/settings.py
+++ core/settings.py
@@ -79,10 +79,15 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'api',
+        'USER': 'admin',
+        'PASSWORD': 'admin123',
+        'HOST': 'db',
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
